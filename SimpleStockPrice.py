58151b5a2e0c7f66257445e39945a88db8b7f373
Andres Felipe Barrero <felipe.barrero17@gmail.com>
2023-03-21 14:28:08 -0500

    Final code

--- SimpleStockPrice.py
+++ SimpleStockPrice.py
@@ -2,17 +2,15 @@ import yfinance as yf
 import streamlit as st
 import pandas as pd
 
-# For this script, I use VOO, but you can use other ticker, lo GOOGl, for Google, or AAPL, for Apple.
+# For this script, I use VOO, but you can use other ticker, like GOOGl, for Google, or AAPL, for Apple.
 
 
 st.write(
     """
 # Simple Stock Price App
-
 ### Show are the stock *closing price* and *volume* of VOO! (Vanguard S&P 500 ETF)
 
 The Vanguard S&P 500 ETF (VOO) is a fund that invests in the stocks of some of the largest companies in the United States. VOO is an exchange-traded fund (ETF) that tracks the S&P 500 index by owning all of the equities within the S&P 500. The S&P 500's investment return is considered a gauge of the overall U.S. stock market.
-
     """
 )
 
@@ -25,11 +23,13 @@ tickerData = yf.Ticker(tickerSymbol)
 # Get the historical prices of this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2023-03-20')
 
+# Chart the stock price at the end of that day/month/year
 st.write("""
 ## Closing Price
 """)
 st.line_chart(tickerDf.Close)
 
+# Chart how many shares were traded that day/month/year
 st.write("""
 ## Volume Price
 """)
